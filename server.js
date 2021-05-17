a9a36698f926c1ad5240444b358c333ff9c1e492
Glitch (fresh-boggy-anaconda) <none>
2021-05-17 07:57:51 +0000

    📯👑 Updated with Glitch

--- server.js
+++ server.js
@@ -1,22 +1,37 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+"use strict";
+require("dotenv").config();
+const session = require("express-session");
+const passport = require("passport");
+const express = require("express");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'pug')
+app.set("view engine", "pug");
+app.use(passport.session());
+app.use(passport.initialize());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  })
+);
 
-app.route('/').get((req, res) => {
-  res.render('index');
+app.route("/").get((req, res) => {
+  res.render(process.cwd() + "/views/index", {
+    title: "Hello",
+    message: "Please login"
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log("Listening on port " + PORT);
 });

28d7bb98714078f496a2a8d9176565ec6285c171
Glitch (fresh-boggy-anaconda) <none>
2021-05-19 15:48:18 +0000

    🌛🐒 Updated with Glitch

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const session = require("express-session");
 const passport = require("passport");
 const express = require("express");
+const ObjectID = require("mongodb").ObjectID;
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
@@ -13,8 +14,7 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
-app.use(passport.session());
-app.use(passport.initialize());
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -24,12 +24,45 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.route("/").get((req, res) => {
   res.render(process.cwd() + "/views/index", {
     title: "Hello",
     message: "Please login"
   });
 });
+
+myDB(async client => {
+  const myDataBase = await client.db("database").collection("users");
+
+  // Be sure to change the title
+  app.route("/").get((req, res) => {
+    //Change the response to render the Pug template
+    res.render("pug", {
+      title: "Hello!",
+      message: "Please login"
+    });
+  });
+
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDB.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+  // Be sure to add this...
+}).catch(e => {
+  app.route("/").get((req, res) => {
+    res.render("pug", { title: e, message: "Unable to login" });
+  });
+});
+// app.listen out here...
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

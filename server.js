807803e1a015a982db71d65a9f17915d85009fb6
Glitch (fresh-boggy-anaconda) <none>
2021-05-19 18:23:24 +0000

    🎾🤘 Updated with Glitch (Registration of New Users)

--- server.js
+++ server.js
@@ -37,25 +37,67 @@ myDB(async client => {
     res.render(process.cwd() + "/views/index", {
       title: "Connected to Database",
       message: "Please login",
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
-  
-  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
-    res.redirect('/profile');
+
+  app
+    .route("/login")
+    .post(
+      passport.authenticate("local", { failureRedirect: "/" }),
+      (req, res) => {
+        res.redirect("/profile");
+      }
+    );
+
+  app.route("/profile").get(ensureAuthenticated, (req, res) => {
+    res.render(process.cwd() + "/views/pug/profile", {
+      username: req.user.username
+    });
   });
 
-  app.route('/profile').get(ensureAuthenticated, (req, res) => {
-    res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
   });
   
-   app.route('/logout').get((req, res) => {
-    req.logout();
-    res.redirect('/');
-  });
+  app.route('/register')
+  .post((req, res, next) => {
+    myDataBase.findOne({ username: req.body.username }, function(err, user) {
+      if (err) {
+        next(err);
+      } else if (user) {
+        res.redirect('/');
+      } else {
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: req.body.password
+        },
+          (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              // The inserted document is held within
+              // the ops property of the doc
+              next(null, doc.ops[0]);
+            }
+          }
+        )
+      }
+    })
+  },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
 
   app.use((req, res, next) => {
-    res.status(404).type('text').send('Not Found');
+    res
+      .status(404)
+      .type("text")
+      .send("Not Found");
   });
 
   // Serialization and deserialization here...
@@ -97,7 +139,7 @@ function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('/');
+  res.redirect("/");
 }
 
 // app.listen out here...

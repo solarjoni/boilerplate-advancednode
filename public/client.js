d1b327711b8ed39c230950af7d29f824f4ee22ad
Glitch (fresh-boggy-anaconda) <none>
2021-05-21 11:26:44 +0000

    🎖🌪 Updated with Glitch (Communicate by Emitting)

--- public/client.js
+++ public/client.js
@@ -1,11 +1,16 @@
-$(document).ready(function () {
+$(document).ready(function() {
   /* Global io */
   let socket = io();
-  // Form submittion with new message in field with id 'm'
-  $('form').submit(function () {
-    var messageToSend = $('#m').val();
 
-    $('#m').val('');
+  socket.on("user count", function(data) {
+    console.log(data);
+  });
+  
+  // Form submittion with new message in field with id 'm'
+  $("form").submit(function() {
+    var messageToSend = $("#m").val();
+
+    $("#m").val("");
     return false; // prevent form submit from refreshing page
   });
 });
